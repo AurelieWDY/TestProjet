3340b9eb4060f0cac413a6a520f82f64cb98847f
Aurélie WANG <Aurlie WANG@DESKTOP-8QMDLRL>
2020-04-01 01:19:32 +0200

    Add class

---
+++ SPA Annecy/src/class_java/MonSI.java
@@ -0,0 +1,11 @@
+package class_java;
+
+public class MonSI {
+
+	public static void main(String[] args) {
+		String s = "Mon app fonctionne !";
+		for (int i = 0; i < s.length();i++){
+		System.out.print(s.charAt(i));
+	}
+	}
+}
