c458b780efe21ac91f955f2b7bcc26933d27794a
Aurélie WANG <Aurlie WANG@DESKTOP-8QMDLRL>
2020-04-01 01:07:31 +0200

    Add class

---
+++ SPA Annecy/src/class_java/Test.java
@@ -0,0 +1,11 @@
+package class_java;
+
+public class Test {
+
+	public static void main(String[] args) {
+		String s = "Mon app fonctionne !";
+		for (int i = 0; i < s.length();i++){
+		System.out.println(s.charAt(i));
+	}
+	}
+}
